9d2c757a4eb62e26df0ca426ea595d2c0324fb48
arya <aryabr99@gmail.com>
2023-04-10 12:45:58 +0530

    integration of github

--- githubdemo/src/pack/Sample.java
+++ githubdemo/src/pack/Sample.java
@@ -4,7 +4,8 @@ public class Sample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+System.out.println("welcome");
+System.out.println("integration of github with selenium");
 	}
 
 }
